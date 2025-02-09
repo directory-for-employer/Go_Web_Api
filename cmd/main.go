0582583b42f34abf5509a5406d2931d22751dee5
ymca <viacheslav.karpunin28@gmail.com>
2025-02-09 05:23:16 -0800

    add gorm and make auto migrate

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"go/web-api/configs"
 	"go/web-api/internal/auth"
+	"go/web-api/pkg/db"
 	"net/http"
 )
 
 func main() {
 	conf := configs.LoadConfig()
+	_ = db.NewDb(conf)
 	router := http.NewServeMux()
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config: conf,
---
+++ migrations/auto.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"go/web-api/internal/link"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic("Error connecting to database")
+	}
+	err = db.AutoMigrate(&link.Link{})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
